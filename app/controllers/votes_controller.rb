a8fe6e5c0e34da6a0260a64be3a6b19eec22cc7e
Suyin Du Bois <suyin.dubois@gmail.com>
2014-04-30 14:22:15 +0100

    Prevented users from voting twice for the same story

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,23 @@ class VotesController < ApplicationController
 	def create
 		#find the right story
 		@story = Story.find(params[:story_id])
+
 		# actually create the vote
 		@vote = @story.votes.new
 
-		@vote.save 
+		# save the IP address of the person voting
+		@vote.ip_address = request.ip 
 
-		flash[:success] = "Thanks for voting"
+		if @vote.save 
+
+			flash[:success] = "Thanks for voting"
+
+			else
+
+			flash[:error] = "You've already voted for this story"
+
+		end
+
 
 		redirect_to story_path(@story)
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,7 @@ class Vote < ActiveRecord::Base
 
 	belongs_to :story, counter_cache: true
 
+	# ip_address must be unique for each story 
+	validates :ip_address, uniqueness: { scope: :story_id }
+
 end
